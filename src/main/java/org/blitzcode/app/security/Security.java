167dd52931d6efe020cfd224aab81cbf4ec5b5ee
Ahmet <35678867+ahmetmutlugun@users.noreply.github.com>
2023-09-29 23:11:22 -0700

    Add spring security

--- src/main/java/org/blitzcode/app/security/Security.java
+++ src/main/java/org/blitzcode/app/security/Security.java
@@ -1,9 +1,10 @@
-package org.blitzcode.app;
+package org.blitzcode.app.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,14 +15,13 @@ public class Security {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
                         .permitAll()
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
