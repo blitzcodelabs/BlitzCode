00aced1f30d3b790b1da61ce9278874609d164b9
Ahmet <35678867+ahmetmutlugun@users.noreply.github.com>
2023-11-02 14:33:03 -0700

    init spring graphql

---
+++ src/main/java/org/blitzcode/app/BlitzCodeApplication.java
@@ -0,0 +1,13 @@
+package org.blitzcode.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlitzCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlitzCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/blitzcode/app/BlitzCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blitzcode.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlitzCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
