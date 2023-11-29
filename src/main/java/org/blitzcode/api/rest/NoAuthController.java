cdda57abf494a19ad1250d3bfeca3e60580a0bb0
airsquared <36649395+airsquared@users.noreply.github.com>
2023-11-28 19:49:56 -0800

    Add supported languages endpoint

--- src/main/java/org/blitzcode/api/rest/NoAuthController.java
+++ src/main/java/org/blitzcode/api/rest/NoAuthController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NoAuthController {
 
+    public record Language(String name, String imageFile) {}
+
     @GetMapping("/languages")
-    public String[] getLanguages() {
-        return new String[] { "Java", "Python", "C++", "C", "C#", "JavaScript"};
+    public Language[] getSupportedBaseLanguages() {
+        return new Language[] {
+                new Language("Java", "java.png"),
+                new Language("Python", "python.png"),
+                new Language("JavaScript", "javascript.png")
+        };
     }
 }
