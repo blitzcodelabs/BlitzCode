486eb87d4f75772d642b44b1295c001e70f27653
airsquared <36649395+airsquared@users.noreply.github.com>
2023-11-29 02:47:28 -0800

    Properly verify Firebase JWT properly

--- src/main/java/org/blitzcode/api/WebSecurityConfiguration.java
+++ src/main/java/org/blitzcode/api/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.blitzcode.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,13 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/**").authenticated()
-                                .anyRequest().anonymous()
-                        )
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwt ->
-                                jwt.jwkSetUri("https://www.googleapis.com/service_accounts/v1/jwk/securetoken%40system.gserviceaccount.com")));
-        return http.build();
+        return http.authorizeHttpRequests(a ->
+                        a.requestMatchers("/auth/**").authenticated().anyRequest().anonymous())
+                .oauth2ResourceServer(o ->
+                        o.jwt(Customizer.withDefaults())) // TODO: look into using firebase admin sdk
+                .build();
     }
 }
