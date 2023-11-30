163faca32726be5f00f3eb1f4aab655c8ad09229
airsquared <36649395+airsquared@users.noreply.github.com>
2023-11-29 23:45:18 -0800

    Set up CORS for localhost:3000

--- src/main/java/org/blitzcode/api/WebSecurityConfiguration.java
+++ src/main/java/org/blitzcode/api/WebSecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +23,15 @@ public class WebSecurityConfiguration {
                         o.jwt(Customizer.withDefaults())) // TODO: look into using firebase admin sdk
                 .csrf(c -> c.disable()) // look into whether this is the right thing to do
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
