e35805b702316b1a16cd00efaa8aee9b1235ad0d
airsquared <36649395+airsquared@users.noreply.github.com>
2023-12-01 14:24:13 -0800

    Add post/get paths with auth

--- frontend/src/lib/request.ts
+++ frontend/src/lib/request.ts
@@ -4,14 +4,18 @@ const url = "http://localhost:8080"
 
 const publicGetPaths = ["/languages"] as const;
 const publicPostPaths = ["/signin", "/signup", "/refresh-token", "/send-reset-password-email"] as const;
-type GetPath = typeof publicGetPaths[number];
-type PostPath = typeof publicPostPaths[number]
+const authGetPaths = ["/test", "/modules", "/lessons", "/account/baseLanguage", "/account/targetLanguage"] as const;
+const authPostPaths = ["/account/baseLanguage", "/account/targetLanguage"] as const;
+type PublicGetPath = typeof publicGetPaths[number];
+type PublicPostPath = typeof publicPostPaths[number]
+type AuthGetPath = typeof authGetPaths[number];
+type AuthPostPath = typeof authPostPaths[number];
 
-export const get = async (path: GetPath) => {
+export const get = async (path: PublicGetPath) => {
     return await (await fetch(url + path)).json();
 }
 
-export const post = async (path: PostPath, data: BodyInit) => {
+export const post = async (path: PublicPostPath, data: BodyInit) => {
     return await fetch(url + path, {
         method: "POST",
         body: data,
@@ -21,10 +25,10 @@ export const post = async (path: PostPath, data: BodyInit) => {
     });
 }
 
-export const getWithAuth = async (path: GetPath) => {
+export const getWithAuth = async (path: AuthGetPath) => {
     const idToken = await getidToken();
     if (idToken) {
-        return await fetch(url + path, {
+        return await fetch(url+ "/auth" + path, {
             headers: {
                 "Authorization": "Bearer " + idToken
             }
@@ -33,10 +37,10 @@ export const getWithAuth = async (path: GetPath) => {
     return null; // TODO: redirect to login page
 }
 
-export const postWithAuth = async (path: PostPath, data: BodyInit) => {
+export const postWithAuth = async (path: AuthPostPath, data: BodyInit) => {
     const idToken = await getidToken();
     if (idToken) {
-        return await fetch(url + path, {
+        return await fetch(url + "/auth" + path, {
             method: "POST",
             body: data,
             headers: {
