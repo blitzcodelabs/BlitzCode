e7d0a8815153ee26461af88b9c2a4a88c9b3f38c
airsquared <36649395+airsquared@users.noreply.github.com>
2023-12-01 12:56:58 -0800

    frontend: add getWithAuth(...), postWithAuth(...), refreshToken()

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -5,16 +5,17 @@ import { cookies } from "next/headers"
 import { SignUpSchema } from "@/components/SignUp";
 import { post } from "./request";
 
-interface User {
+export interface User {
     idToken: string;
     refreshToken: string;
+    expiry: Date;
 }
 
 export const login = async (credentials: LoginSchema) => {
     const res = await post("/signin", JSON.stringify(credentials));
     if (res.ok) {
         const data = await res.json();
-        cookies().set("user", JSON.stringify(data));
+        updateUser(data.idToken, data.refreshToken);
     }
     return res.ok;
 }
@@ -23,12 +24,46 @@ export const signUp = async (credentials: SignUpSchema) => {
     const res = await post("/signup", JSON.stringify(credentials))
     if (res.ok) {
         const data = await res.json();
-        cookies().set("user", JSON.stringify(data));
+        updateUser(data.idToken, data.refreshToken);
     }
     return res.ok;
+}
+
+export const refreshToken = async () => {
+    const user = getUser();
+    if (user) {
+        const res = await post("/refresh-token", user.refreshToken);
+        if (res.ok) {
+            const {id_token, refresh_token} = await res.json();
+            updateUser(id_token, refresh_token);
+        }
+        return res.ok;
+    }
+    return false;
 }
 
 export const getUser = () => {
     const data = cookies().get("user")?.value;
     return data ? JSON.parse(data) as User : undefined;
+}
+
+export const hasExpired = (user: User) => {
+    return Date.now() > user.expiry.getTime();
+}
+
+export const refreshIfNeeded = async (user: User) => {
+    if (hasExpired(user)) {
+        return await refreshToken();
+    }
+    return true;
+}
+
+const updateUser = (idToken: string, refreshToken: string) => {
+    const user: User = {
+        idToken: idToken,
+        refreshToken: refreshToken,
+        // expires in 3600 seconds (100 second gap for safety)
+        expiry: new Date(Date.now() + 3500 * 1000)
+    };
+    cookies().set("user", JSON.stringify(user));
 }
--- frontend/src/lib/request.ts
+++ frontend/src/lib/request.ts
@@ -1,21 +1,56 @@
+import {refreshIfNeeded, getUser} from "@/lib/auth";
+
 const url = "http://localhost:8080"
 
-const getPaths = ["/languages"] as const;
-const postPaths = ["/signin", "/signup"] as const;
-type GetPath = typeof getPaths[number];
-type PostPath = typeof postPaths[number]
+const publicGetPaths = ["/languages"] as const;
+const publicPostPaths = ["/signin", "/signup", "/refresh-token", "/send-reset-password-email"] as const;
+type GetPath = typeof publicGetPaths[number];
+type PostPath = typeof publicPostPaths[number]
 
 export const get = async (path: GetPath) => {
     return await (await fetch(url + path)).json();
 }
 
 export const post = async (path: PostPath, data: BodyInit) => {
-    const res = await fetch(url + path, {
+    return await fetch(url + path, {
         method: "POST",
         body: data,
         headers: {
             "Content-Type": "application/json"
         }
-    })
-    return res;
+    });
+}
+
+export const getWithAuth = async (path: GetPath) => {
+    const user = getUser();
+    if (user) {
+        if (!await refreshIfNeeded(user)) {
+            return undefined; // TODO: redirect to login page
+        }
+        return await fetch(url + path, {
+            headers: {
+                "Authorization": "Bearer " + user.idToken
+            }
+        });
+    }
+    // TODO: redirect to login page
+    return undefined;
+}
+
+export const postWithAuth = async (path: PostPath, data: BodyInit) => {
+    const user = getUser();
+    if (user) {
+        if (!await refreshIfNeeded(user)) {
+            return undefined; // TODO: redirect to login page
+        }
+        return await fetch(url + path, {
+            method: "POST",
+            body: data,
+            headers: {
+                "Authorization": "Bearer " + user.idToken,
+                "Content-Type": "application/json"
+            }
+        });
+    }
+    return undefined; // TODO: redirect to login page
 }
