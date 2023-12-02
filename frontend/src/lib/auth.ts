b6853bcbf9a2fc83f239375906277638e8cfdb9f
Shane <github@ricedust.com>
2023-12-01 23:23:21 -0800

    Revert "tweaks to auth.ts and request.ts"
    This reverts commit a8f4d5f90c6138debcd866bcb3fbd40f8fa59b05.

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,17 +1,20 @@
+"use client"
+
 import { LoginSchema } from "@/components/Login";
 import { SignUpSchema } from "@/components/SignUp";
 import { post } from "./request";
 
-export const tryGetIDToken = async () => {
-    const idToken = localStorage.getItem("idToken");
-    if (!idToken) return;
-    if (!hasExpired() || await tryRefresh()) return idToken;
+export const getidToken = async () => {
+    if (await refreshIfNeeded()) {
+        return localStorage.getItem("idToken");
+    }
+    return null;
 }
 
 export const login = async (credentials: LoginSchema) => {
     const res = await post("/signin", JSON.stringify(credentials));
     if (res.ok) {
-        const { idToken, refreshToken } = await res.json();
+        const {idToken, refreshToken} = await res.json();
         updateTokens(idToken, refreshToken);
     }
     return res.ok;
@@ -20,13 +23,15 @@ export const login = async (credentials: LoginSchema) => {
 export const signUp = async (credentials: SignUpSchema) => {
     const res = await post("/signup", JSON.stringify(credentials))
     if (res.ok) {
-        const { idToken, refreshToken } = await res.json();
+        const {idToken, refreshToken} = await res.json();
         updateTokens(idToken, refreshToken);
     }
     return res.ok;
 }
 
-export const logout = () => localStorage.clear();
+export const logout = () => {
+    localStorage.clear();
+}
 
 const updateTokens = (idToken: string, refreshToken: string) => {
     localStorage.setItem("idToken", idToken);
@@ -35,22 +40,22 @@ const updateTokens = (idToken: string, refreshToken: string) => {
     localStorage.setItem("expiry", (Date.now() + 3540 * 1000).toString());
 }
 
-const hasExpired = () => {
-    return Date.now() > Number(localStorage.getItem("expiry"));
+const refreshIfNeeded = async () => {
+    if (hasExpired()) {
+        const refreshToken = localStorage.getItem("refreshToken");
+        if (!refreshToken) {
+            return false;
+        }
+        const res = await post("/refresh-token", refreshToken);
+        if (!res.ok) {
+            return false;
+        }
+        const {id_token, refresh_token} = await res.json();
+        updateTokens(id_token, refresh_token);
+    }
+    return !hasExpired();
 }
 
-const tryRefresh = async () => {
-    const refreshToken = localStorage.getItem("refreshToken");
-    if (!refreshToken) {
-        return false;
-    }
-
-    const res = await post("/refresh-token", refreshToken);
-    if (!res.ok) {
-        return false;
-    }
-
-    const { id_token, refresh_token } = await res.json();
-    updateTokens(id_token, refresh_token);
-    return true;
+const hasExpired = () => {
+    return Date.now() > Number(localStorage.getItem("expiry") || "0");
 }
