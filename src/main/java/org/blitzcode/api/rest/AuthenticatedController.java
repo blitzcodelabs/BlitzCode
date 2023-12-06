eb78f11743785d2e68229009f49ac4d2e182460c
Ahmet <35678867+ahmetmutlugun@users.noreply.github.com>
2023-12-05 17:18:04 -0800

    Added backend api for new placeholder json

--- src/main/java/org/blitzcode/api/rest/AuthenticatedController.java
+++ src/main/java/org/blitzcode/api/rest/AuthenticatedController.java
@@ -1,6 +1,10 @@
 package org.blitzcode.api.rest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
 import org.blitzcode.api.controller.UserController;
 import org.blitzcode.api.model.Language;
 import org.blitzcode.api.rest.ResponseTypes.ModuleEntry;
@@ -12,9 +16,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/auth")
@@ -53,12 +61,52 @@ public class AuthenticatedController {
 
     @GetMapping(path = "/questions/{lessonID}")
     public Question[] getQuestions(@PathVariable String lessonID, JwtAuthenticationToken token) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            // Adjust the file path as needed
+            File file = new File("src/main/resources/JavaToPython/modules/Essentials/Printing.json");
+            List<QuestionItem> quizItems = mapper.readValue(file, new TypeReference<>() {});
+
+            Question[] questions = new Question[quizItems.size()];
+            // Now you can use the quizItems list
+            for (int i = 0; i < questions.length; i++) {
+                QuestionItem item = quizItems.get(i);
+                int answerIndex = insertStringAtRandomIndex(item.wrongAnswers, item.answer);
+                String[] arr = item.wrongAnswers.toArray(new String[0]);
+                questions[i] = new Question(item.question, answerIndex, arr);
+            }
+
+            return questions;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Question[]{
                 new Question("int x = 5;", 2, "const x = 5;", "int x = 5;", "let x = 5;"),
                 new Question("String message = \"Hello\";", 1, "String message = \"Hello\";", "let message = \"Hello\";", "var message = \"Hello\";"),
                 new Question("double price = 19.99;", 0, "let price = 19.99;", "const price = 19.99;", "double price = 19.99;"),
                 new Question("boolean isValid = true;", 2, "const isValid = true;", "boolean isValid = true;", "let isValid = true;")
         };
+    }
+
+    public static int insertStringAtRandomIndex(List<String> stringList, String stringToInsert) {
+        Random random = new Random();
+        int insertIndex = random.nextInt(stringList.size() + 1);
+
+        stringList.add(insertIndex, stringToInsert);
+        return insertIndex;
+    }
+
+    @Getter
+    @Setter
+    private static class QuestionItem
+    {
+        private String question;
+        private String answer;
+        @JsonProperty("wrongAnswers")
+        private List<String> wrongAnswers;
+
+        // getters and setters
     }
 
     @PostMapping(path = "/questions/completed/{lessonID}")
